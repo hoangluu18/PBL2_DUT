e633e8c82df939690cf359a19c29e77b9fc9c357
Ahrumiki <tranngocminhhoangpct@gmail.com>
2023-10-30 23:32:56 +0700

    đã thêm xuất chiếu

---
+++ suatchieu.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+class Suatchieu {
+    private:
+    string maphim;
+    string TimeBegin;
+    int price;
+
+    public:
+    Suatchieu(string = "", string = "", int = 0);
+    ~Suatchieu();
+
+    void setTimeBegin();
+    void setprice();
+    static void themsuatchieu();
+}; 
