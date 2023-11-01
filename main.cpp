16567245f80af24add81c9f9f6e3252686738689
Lưu Việt Hoàng <luu186835@gmail.com>
2023-11-01 17:12:44 +0700

    thêm test case

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include "staffManager.h"
 using namespace std;
 
-bool check ;
+
 void intro()
 {
     system("cls");
@@ -24,44 +24,89 @@ void intro()
 	getch();
 }
 
-void choice()
-{
-
+void intro_manager()
+{    
+	system("cls");
+	cout <<"\t\t\t"<<"|===========================|" << endl;
+	cout <<"\t\t\t"<<"|     	MANAGEMENT          |"<< endl;
+	cout <<"\t\t\t"<<"|  1. Movie Management      |" << endl;
+	cout <<"\t\t\t"<<"|  2. Staff Managemnt       |" << endl;
+	cout <<"\t\t\t"<<"|  3. Exit                  |" << endl;
+	cout <<"\t\t\t"<<"|===========================|" << endl;
+    cout<<"\t\t\tChoose: ";
+     
 }
 
-void login()
-{ 
-    accManagement acc;
-	acc.checkLogin();
-}
 
-void movieManager()
-{
-	movieManagement movie;
-	movie.start();
-}
 
 void run()
 {   
-	intro();
-	login();
-	movieManager();
-	Suatchieu sc;
-	sc.insuatchieucuaphim("MP1");
-// }
+	 
+	do
+	{ 
+	  bool check ,out_the_loop = false;// điều kiện thoát vòng lặp
+	  intro();
+	  accManagement acc;
+	  acc.checkLogin(check);// check = true gọi đến quản lí , false gọi đến nhân viên đặt vé
+      if(check == true)
+	  { 
+		
+		while(out_the_loop == false)
+		{
+       		char tt; // lựa chọn 1 là quản lí phim, 2 là quản lí nhân viên
+			intro_manager();
+
+			tt = getche();
+       		switch (tt)
+			{
+		   
+				case '1':
+				{
+					movieManagement movie;
+					movie.start();
+					break;
+				}
+
+				case '2':
+				{
+					//đang phát triển
+				    //staffManager staff;
+					//staff.start();
+					break;
+				}
+
+				case '3':
+				{   
+					out_the_loop = true;
+					break;
+				}
+
+				default: cout<<"\a";
+			
+			} 
+		}
+		
+	  }
+
+	  else
+	  {
+           // gọi đến hàm booking.start(); // đang phát triển
+
+
+	  }
+
+		
+	} while (1);
+	
 }
+
 
 int main()
 {    
-    run();
-    cout<<"\nhello";
+     
+	run();
 
-   staffManager sm;
-   sm.readFile();
-   sm.printListStaff();
-   //sm.removeStaff("MNV01");
-   sm.printListStaff();
-   sm.writeFile();
+	return 0;
 }
 
 
